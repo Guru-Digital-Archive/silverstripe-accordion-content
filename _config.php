ab2068cb3e074f5d6d2c64396a574b534315cc2c
corey <corey@gdmedia.tv>
2014-09-10 12:42:00 +1200

    Update description

--- _config.php
+++ _config.php
@@ -3,5 +3,5 @@
 HtmlEditorConfig::get('cms')->enablePlugins('template');
 HtmlEditorConfig::get('cms')->insertButtonsAfter('tablecontrols', 'template');
 HtmlEditorConfig::get('cms')->setOptions(array('template_templates' => array(
-        array('title' => '3 Columns', 'src' => 'silverstripe-accordian-content/templates/accordian.html', 'description' => '4 Section accordian')
+        array('title' => '4 Section accordian', 'src' => 'silverstripe-accordian-content/templates/accordian.html', 'description' => 'Add a 4 section accordion content to your page')
 )));
